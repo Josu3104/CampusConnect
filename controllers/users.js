c82b80ac08359c22f367d20b02dbd29da47cca2a
Gavidia <jxsuemanuel0331@gmail.com>
2025-11-19 17:06:21 -0600

    Primeros endpoints

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,48 @@
-const { Usuarios } = require("../models");
+const {Usuarios} = require('../models')
 
-const createNewUser = async (req, res) => {
-  try {
-    const repeatEmail = await Usuarios.findOne({
-      correo: req.body.correo,
-    });
-    if (!repeatEmail) {
-      const newUser = await Usuarios.create(req.body);
-      res.status(201).json();
-    } else {
-      res.status(409).json({
-        status: "Email Conflict",
-        message: "The email is already registered, try with another one.",
-      });
+const createNewUser = async (request, response) => {
+    try{
+        const data = request.body;
+        if (!data || Object.keys(data).length === 0) {
+            return response.status(400).json({
+                status: "Error",
+                message: "Request body cannot be empty"
+            });
+        }
+        const check = await Usuarios.findOne({
+            correo: data.correo
+        });
+        if (!check){
+            const newUser = await Usuarios.create(request.body);
+            response.status(201).json({
+                status: "Success",
+                mesage: newUser
+            })
+        }
+        else {
+            response.status(409).json({
+                status: "Email Conflict",
+                message: "Email already exists, please use another email"
+            })
+        }
+    } catch (error) {
+        response.status(500).json({
+                status: "Error",
+                message: error.message
+            })
     }
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message,
-    });
-  }
+
 };
+
+const getAllUsers = async (request, response) => {
+    try{
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = {
+    createNewUser,
+    
+}
